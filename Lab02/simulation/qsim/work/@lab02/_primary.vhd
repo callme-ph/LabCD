0a25ea47adf1805692d31667fad9d209889726d1
Pedro <113944862+callme-ph@users.noreply.github.com>
2024-03-16 13:37:05 -0300

    Add files via upload

---
+++ Lab02/simulation/qsim/work/@lab02/_primary.vhd
@@ -0,0 +1,10 @@
+library verilog;
+use verilog.vl_types.all;
+entity Lab02 is
+    port(
+        s1              : out    vl_logic;
+        c               : in     vl_logic;
+        b               : in     vl_logic;
+        a               : in     vl_logic
+    );
+end Lab02;
---
+++ Lab02/simulation/qsim/work/@lab02_vlg_check_tst/_primary.vhd
@@ -0,0 +1,8 @@
+library verilog;
+use verilog.vl_types.all;
+entity Lab02_vlg_check_tst is
+    port(
+        s1              : in     vl_logic;
+        sampler_rx      : in     vl_logic
+    );
+end Lab02_vlg_check_tst;
