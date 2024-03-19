eabc4698143539c2073483bf6f03c30c19a96d7a
Pedro <113944862+callme-ph@users.noreply.github.com>
2024-03-19 19:41:21 -0300

    Delete Lab02 directory

--- Lab02/simulation/qsim/work/@lab02_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Lab02_vlg_check_tst is
-    port(
-        s1              : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end Lab02_vlg_check_tst;
--- Lab02/simulation/qsim/work/@lab02_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Lab02_vlg_vec_tst is
-end Lab02_vlg_vec_tst;
